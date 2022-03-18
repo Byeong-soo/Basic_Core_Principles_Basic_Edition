f71493bdaf30a9c174b8752bd41736ee5fc5b5ba
byeongSoo <ubs4939@naver.com>
2022-03-18 23:40:28 +0900

    @primary로 주입

--- src/main/java/hello/demo/order/OrderServiceImpl.java
+++ src/main/java/hello/demo/order/OrderServiceImpl.java
@@ -25,9 +25,9 @@ public class OrderServiceImpl implements OrderService{
 //    }
 
         @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy rateDiscountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = rateDiscountPolicy;
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
